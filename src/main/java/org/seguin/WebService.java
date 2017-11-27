3b4cea56b7a75117c51e7c75f80896889b576a66
Séguin Gabriel <1507779@cegepmontpetit.ca>
2017-11-27 18:08:19 -0500

    static tokenlist bug

--- src/main/java/org/seguin/WebService.java
+++ src/main/java/org/seguin/WebService.java
@@ -22,11 +22,15 @@ public class WebService {
     static IngredientList allIngredients = new IngredientList();
     static ArrayList<Ingredients> ingredientList = new ArrayList<Ingredients>(allIngredients);
     public static final String Cookie="Drinks_R_Uscookie";
-    static ArrayList<Token> tokenList = new ArrayList<Token>();
+     ArrayList<Token> tokenList = new ArrayList<Token>();
     //region User Related
 
     @GET @Path("/Users/GetUser/{id}")
-    public Users getUserById(@CookieParam(Cookie) Cookie cookie, @PathParam("id") int id){
+    public Users getUserById(@CookieParam(Cookie) Cookie cookie, @PathParam("id") int id) throws InterruptedException {
+        Thread.sleep(2000);
+        if (!cookieIsValid(cookie))
+            return null;
+
         System.out.println("Get getuserbyid id: " + id);
 
         for (Users user :
@@ -40,17 +44,27 @@ public class WebService {
 
     }
 
-    @GET @Path("/Users/Logout")
-    public Response logoutUser(@CookieParam(Cookie) Cookie cookie){
+    @GET @Path("/Users/Logout/{id}")
+    public Response logoutUser(@CookieParam(Cookie) Cookie cookie) throws InterruptedException {
+        Thread.sleep(2000);
         System.out.println("Get logoutuser id: ");
-        tokenList.remove(tokenList.indexOf(cookie.getValue()));
+        for (Token tok :
+                tokenList) {
+            if (tok.TokenId.equals(cookie.getValue())){
+                tokenList.remove(tok);
+            }
+        }
 
         NewCookie cookieasupprimer = new NewCookie(Cookie, null, "/", null, null, 0, true);
         return Response.ok(new Gson().toJson(true), MediaType.APPLICATION_JSON).cookie(cookieasupprimer).build();
     }
 
     @GET @Path("/Users/MyDrinks/{id}")
-    public ArrayList<Drinks> getMyDrinks(@CookieParam(Cookie) Cookie cookie, @PathParam("id") int id){
+    public ArrayList<Drinks> getMyDrinks(@CookieParam(Cookie) Cookie cookie, @PathParam("id") int id) throws InterruptedException {
+        Thread.sleep(2000);
+        if (!cookieIsValid(cookie))
+            return null;
+
         System.out.println("Get getmydrinks id: " + id);
 
         Users user = getUserById(cookie, id);
@@ -59,7 +73,11 @@ public class WebService {
     }
 
     @GET @Path("/Users/MyFavs/{id}")
-    public ArrayList<Drinks> getMyFavs(@CookieParam(Cookie) Cookie cookie, @PathParam("id") int id){
+    public ArrayList<Drinks> getMyFavs(@CookieParam(Cookie) Cookie cookie, @PathParam("id") int id) throws InterruptedException {
+        Thread.sleep(2000);
+        if (!cookieIsValid(cookie))
+            return null;
+
         System.out.println("Get getmyfavs id: " + id);
 
         Users user = getUserById(cookie, id);
@@ -69,7 +87,11 @@ public class WebService {
 
     @POST
     @Path("/Users/AddDrink")
-    public String addMyDrinks(@CookieParam(Cookie) Cookie cookie, UserDrink drink){
+    public String addMyDrinks(@CookieParam(Cookie) Cookie cookie, UserDrink drink) throws InterruptedException {
+        Thread.sleep(2000);
+        if (!cookieIsValid(cookie))
+            return null;
+
         System.out.println("POST addmydrinks drink: " + drink.drink.getName() + " userid: " + drink.user.getId());
 
         Users user = getUserById(cookie, drink.user.getId());
@@ -78,7 +100,11 @@ public class WebService {
     }
 
     @POST @Path("/Users/AddFav")
-    public String addMyFavs(@CookieParam(Cookie) Cookie cookie, UserDrink fav){
+    public String addMyFavs(@CookieParam(Cookie) Cookie cookie, UserDrink fav) throws InterruptedException {
+        Thread.sleep(2000);
+        if (!cookieIsValid(cookie))
+            return null;
+
         System.out.println("POST addMyFavs drink: " + fav.drink + " userid: " + fav.user.getId());
 
         Users user = getUserById(cookie, fav.user.getId());
@@ -87,7 +113,8 @@ public class WebService {
     }
 
     @POST @Path("/Users/VerifyCredentials")
-    public Response verifyCredentials(EmailPassword login){
+    public Response verifyCredentials(EmailPassword login) throws InterruptedException {
+        Thread.sleep(2000);
         /*//testing
         Token token = new Token();
         token.UserId = Integer.toString(0);
@@ -123,7 +150,8 @@ public class WebService {
     }
 
     @POST @Path("/Users/Create")
-    public Users createUser(EmailPassword credentials){
+    public Response createUser(EmailPassword credentials) throws InterruptedException {
+        Thread.sleep(2000);
         System.out.println("POST createUser emailpassword: " + credentials.email + " " + credentials.password);
 
         for (Users user:
@@ -138,9 +166,16 @@ public class WebService {
         Token token = new Token();
         token.UserId = Integer.toString(user.getId());
         token.TokenId = UUID.randomUUID().toString();
+        //create expiration date for the token 15 days later from creation
+        token.dateExpiration = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(token.dateExpiration);
+        calendar.add(Calendar.DATE, 15);
+        token.dateExpiration = calendar.getTime();
+
         tokenList.add(token);
         NewCookie cookie = new NewCookie(Cookie, token.TokenId, "/", null, "id token", 64800, true);
-        return user;
+        return Response.ok(new Gson().toJson(token), MediaType.APPLICATION_JSON).cookie(cookie).build();
 
     }
 
@@ -149,7 +184,11 @@ public class WebService {
     //region Drink Related
 
     @POST @Path("/Drinks/Create")
-    public String createDrink(@CookieParam(Cookie) Cookie cookie, Drinks drink){
+    public String createDrink(@CookieParam(Cookie) Cookie cookie, Drinks drink) throws InterruptedException {
+        Thread.sleep(2000);
+        if (!cookieIsValid(cookie))
+            return null;
+
         System.out.println("POST createdrink drink: " + drink.getName());
 
         drinkList.add(drink);
@@ -157,7 +196,11 @@ public class WebService {
     }
 
     @GET @Path("/Drinks/GetAllDrinks")
-    public ArrayList<Drinks> getAllDrinks(@CookieParam(Cookie) Cookie cookie){
+    public ArrayList<Drinks> getAllDrinks(@CookieParam(Cookie) Cookie cookie) throws InterruptedException {
+        Thread.sleep(2000);
+        if (!cookieIsValid(cookie))
+            return null;
+
         System.out.println("GET getalldrink ");
 
         return drinkList;
@@ -195,7 +238,8 @@ public class WebService {
     //region Ingredient Related
 
     @POST @Path("/Ingredients/Create")
-    public String createIngredient(@CookieParam(Cookie) Cookie cookie, Ingredients ingr){
+    public String createIngredient(@CookieParam(Cookie) Cookie cookie, Ingredients ingr) throws InterruptedException {
+        Thread.sleep(2000);
         System.out.println("POST createingredient ingredient: " + ingr.Nom);
 
         ingredientList.add(ingr);
@@ -233,11 +277,34 @@ public class WebService {
     */
 
     @GET @Path("/Ingredients/GetAllIngredient")
-    public ArrayList<Ingredients> getAllIngredients(@CookieParam(Cookie) Cookie cookie){
+    public ArrayList<Ingredients> getAllIngredients(@CookieParam(Cookie) Cookie cookie) throws InterruptedException {
+        Thread.sleep(2000);
         System.out.println("GET getallingredients");
         return ingredientList;
     }
 
     //endregion
 
+    private boolean cookieIsValid(Cookie cookie){
+        for (Token tok :
+                tokenList) {
+            if (cookie.getValue().equals(tok.TokenId)) {
+                if (tokenIsValid(tok))
+                    return true;
+                else{
+                    tokenList.remove(tok);
+                    return false;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    private boolean tokenIsValid(Token token){
+        if (token.dateExpiration.before(new Date()))
+        return false;
+
+        return true;
+    }
 }
